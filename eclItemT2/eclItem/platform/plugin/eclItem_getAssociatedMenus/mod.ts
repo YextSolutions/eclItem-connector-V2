90d2ffe63e77fba915fe533c1ff5ea44556a9ac1
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-03 12:51:30 -0400

    Add files via upload

---
+++ eclItemT2/eclItem/platform/plugin/eclItem_getAssociatedMenus/mod.ts
@@ -0,0 +1,41 @@
+//This function takes in a menu item name as an argument and returns an object with duplicate items and the names of the menus that contain them
+export async function getMenuArray(menuItemName) {
+
+
+  // Initialize offset and API endpoint URL for initial call
+  let offset = 0;
+  let totalMenusCount = 1;
+
+  // Initialize empty array to store duplicate items and associated menu names
+  const menuArray = [];
+
+  // Make initial API call to retrieve first page of results and convert response to JSON format
+ 
+
+  // While there are still menus left to retrieve, continue making API calls
+  while (offset < totalMenusCount) {
+
+    let apiUrl = 'https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=6de480af557d3649a27b4ce187346023&limit=50&offset=' + offset; //do we need to use +offset instead? The expression 
+
+    let response = await fetch(apiUrl);
+    let jsonResponse = await response.json();
+     // Retrieve total count of menus returned by API
+    totalMenusCount = jsonResponse.response.count;
+    // Loop through menus returned by API response
+    for (let menu of jsonResponse.response.menus) {
+      for (let section of menu.sections) {
+        for (let item of section.items) {
+          if(item.name === menuItemName) {
+            menuArray.push(menu.name)
+          }
+        }
+      // If more than one item with matching name is found, add them to menuArray array with associated menu id
+    }
+  }
+    // Update offset and API endpoint URL for next API call
+    offset = offset + 10; 
+  }
+  var uniqueMenuArray = [...new Set(menuArray)]; // removes duplicates
+  return uniqueMenuArray.toString()
+  // Return an object with the array of duplicate items and their associated menu names
+}
---
+++ eclItemT2/eclItem/platform/plugin/eclItem_isDupe/mod.ts
@@ -0,0 +1,50 @@
+export async function isDupe(idAndName) { 
+  
+  var myIds = idAndName.split(',')
+
+  var id = myIds[0]
+
+  if (myIds.length > 2) {
+    var nameArray = myIds.slice(1)
+    var name = nameArray.toString()
+  }
+  else {
+    var name = myIds[1]
+  }
+
+  let counter = 0;
+  let finalCounter = 0;
+
+  let offset = 0
+  let count = 1
+
+  while (offset < count) {
+
+    const response = await fetch('https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=6de480af557d3649a27b4ce187346023&limit=50&offset=' + offset);  // need pagination but i think you know that
+    const jsonResponse = await response.json();
+
+
+    for (const menu of jsonResponse.response.menus) { 
+      for (const section of menu.sections) {
+        for (const item of section.items) {
+          if (name === item.name) {
+            counter++;
+          }
+          if (id === item.id) {
+            finalCounter = counter;
+          }
+        }
+      }
+    }
+
+    offset = offset + 10
+    count = jsonResponse.response.count
+
+  }
+
+    if (finalCounter > 1) {
+        return 'DUPE';
+      } else if (finalCounter === 1) { 
+        return 'UNIQUE';
+      }
+  }
