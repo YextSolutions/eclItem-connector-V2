42ca9f7eec7bd0f56ae655af2d8ce13f4f55b482
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-09 18:00:44 -0400

    Update mod.ts

--- eclItem/platform/plugin/eclItem_isDupe/mod.ts
+++ eclItem/platform/plugin/eclItem_isDupe/mod.ts
@@ -20,7 +20,7 @@ export async function isDupe(idAndName) {
 
   while (offset < count) {
 
-    const response = await fetch('https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=${apiKey}&limit=50&offset=' + offset);  
+    const response = await fetch('https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=${{apiKey}}&limit=50&offset=' + offset);  
     const jsonResponse = await response.json();
     if(typeof jsonResponse.response.menus == 'undefined') {return;}
 
