1fbadeead0d49ba1d6e62cf5c8be1c5952a084e4
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-18 15:30:20 -0400

    Update mod.ts

--- eclItem/platform/plugin/eclItem_isDupe/mod.ts
+++ eclItem/platform/plugin/eclItem_isDupe/mod.ts
@@ -1,5 +1,4 @@
-export async function isDupe(idAndName) { 
-  
+export async function isDupe(idAndName) {
   var myIds = idAndName.split(',')
 
   var id = myIds[0]
@@ -7,8 +6,7 @@ export async function isDupe(idAndName) {
   if (myIds.length > 2) {
     var nameArray = myIds.slice(1)
     var name = nameArray.toString()
-  }
-  else {
+  } else {
     var name = myIds[1]
   }
 
@@ -19,33 +17,56 @@ export async function isDupe(idAndName) {
   let count = 1
 
   while (offset < count) {
-
-    const response = await fetch('https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=${{apiKey}}&limit=50&offset=' + offset);  
+    const response = await fetch('https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=${{apiKey}}&limit=50&offset=${offset}');
     const jsonResponse = await response.json();
-    if(typeof jsonResponse.response.menus == 'undefined') {return;}
-
-    else {
-    for (const menu of jsonResponse.response.menus) { 
-      for (const section of menu.sections) {
-        for (const item of section.items) {
-          if (name === item.name) {
-            counter++;
-          }
-          if (id === item.id) {
-            finalCounter = counter;
+    if (typeof jsonResponse.response.menus == 'undefined') {
+      return;
+    } else {
+      for (const menu of jsonResponse.response.menus) {
+        for (const section of menu.sections) {
+          for (const item of section.items) {
+            if (name === item.name) {
+              counter++;
+            }
+            if (id === item.id) {
+              finalCounter = counter;
+            }
           }
         }
       }
-    }
 
-    offset = offset + 50
-    count = jsonResponse.response.count
-  }
-  }
+      offset = offset + 50;
+      count = jsonResponse.response.count;
 
-    if (finalCounter > 1) {
-        return 'DUPE';
-      } else if (finalCounter === 1) { 
-        return 'UNIQUE';
+      if (jsonResponse.nextPageToken) {
+        const sleepResult = await sleepUntilRetryOrContinue(jsonResponse.nextPageToken);
+        if (sleepResult) {
+          return sleepResult;
+        }
       }
+    }
   }
+
+  if (finalCounter > 1) {
+    return 'DUPE';
+  } else if (finalCounter === 1) {
+    return 'UNIQUE';
+  }
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sleepUntilRetryOrContinue(pageToken) {
+  if (pageToken) {
+    const state = JSON.parse(pageToken);
+    if (state.retryAfter) {
+      const secondsToSleep = state.retryAfter > 5 ? 5 : state.retryAfter;
+      await sleep(secondsToSleep * 1000);
+      state.retryAfter -= secondsToSleep;
+      return JSON.stringify({ data: { tickets: [{ id: "" }] }, nextPageToken: JSON.stringify(state) });
+    }
+  }
+  return undefined;
+}
